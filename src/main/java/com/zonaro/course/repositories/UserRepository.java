d3ea20057d47977643a4a5ef39d5f1163ea80d77
Matheus Zonaro <matheus>
2021-03-30 11:34:24 -0300

    Service Layer e Component Registration

--- src/main/java/com/zonaro/course/repositories/UserRepository.java
+++ src/main/java/com/zonaro/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.zonaro.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.zonaro.course.entities.User;
 
+@Repository // Nem é ao necessario pq ja herda do jparepository que tem o @repository
 public interface UserRepository extends JpaRepository<User, Long>{ // Pediu o tipo, no caso como é user que to criando, entao user, e o id é long
 	
 	
--- src/main/java/com/zonaro/course/resources/UserResource.java
+++ src/main/java/com/zonaro/course/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.zonaro.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zonaro.course.entities.User;
+import com.zonaro.course.services.UserService;
 
 // São os recursos da aplicação, tnho um objeto user, agr um user resource
 // Controlldores rest, que precisa de uma service e dps data repositories
@@ -14,11 +19,27 @@ import com.zonaro.course.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping // metodo que responde a http
-	public ResponseEntity<User> findAll(){ // RespondeEntity é um generics do spring para retorno do banco
+	public ResponseEntity<List<User>> findAll(){ // RespondeEntity é um generics do spring para retorno do banco
 		
-		User u = new User(1L, "Maria", "maria@hotmail.com", "11975626289", "123456");
-		return ResponseEntity.ok().body(u); // retorno do banco
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list); // retorno do banco
 	}
+	
+	
+	@GetMapping(value = "/{id}") // quer dizer qu aceita id no metodo
+	public ResponseEntity<User> findById(@PathVariable Long id){  // to dizendo que o id daqui vem de cima
+
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
+		
+	}
+	
+	
 
 }
